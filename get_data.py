3ad0887eb4a49754bab7e551e8971a1d142274a6
antonio manganelli <manganelliantonio01@gmail.com>
2019-12-28 13:04:54 +0100

    get_data in main

---
+++ get_data.py
@@ -0,0 +1,17 @@
+from market_data.src.getMarketData import getMarketData
+import market_data.src.util as util
+util.set_logging()
+
+
+
+
+
+
+md = getMarketData()
+md.define_item(market='binance', sym='BTC-USDT', data_type='trades')
+md.define_item(market='binance', sym='ETH-USDT', data_type='trades')
+md.run()
+
+
+
+
